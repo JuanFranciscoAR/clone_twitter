3ffcad4adddc01929432beaa95131ab01201c5c1
JuanFrancisco <juanarmijoreyes@gmail.com>
2023-07-25 10:36:35 -0400

    Seed Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# db/seeds.rb
+require 'faker'
+
+# Crea colaboradores con nombres, correos electrónicos y direcciones aleatorias
+50.times do
+    username = Faker::Name.name
+    description = Faker::Lorem.paragraphs
+
+Clone.create(
+    username: username,
+    description: description,
+
+)
+end
+
+
